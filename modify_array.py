8801bae5cc3edd4d37359bee2976bc31e9082f40
Thomas Ferris <thomaskeithferris@gmail.com>
2019-09-19 20:04:00 -0700

    reduced

--- modify_array.py
+++ modify_array.py
@@ -2,16 +2,12 @@
 # should become ["3-Brigadier Gerard!", "2-Black Caviar!", "1-Abricot du Laudot!"] after formatting.
 
 def format_array(arr):
-  modify = []
   final = []
   counter = len(arr)
   index = 0
 
   for item in arr:
-    modify.append("-" + item + "!")
-
-  for item in modify:
-    final.append(str(counter) + modify[index])
+    final.append(str(counter) + "-" + item + "!")
     index += 1
     counter -= 1
 
