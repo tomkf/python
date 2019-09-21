1ab5b349eb87c57a0c2a676354f302c55a1607ea
Thomas Ferris <thomaskeithferris@gmail.com>
2019-09-20 17:23:35 -0700

    will work if passed exact values

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,5 +1,3 @@
-import itertools
-
 # method that when passed an integer above 0 returns a string containing the proper Roman numeral.
 #  In other words, old_roman_numeral(4) should return "IIII".
 # For reference, these are the values of the letters used:
@@ -27,13 +25,20 @@ def roman_numeral(num):
     
     counter = 0 
     count = 0
+    working_value = 0 
+    first_val = filter(lambda x: x > 0, capture_values)
+    first_occurrence= list(first_val)[0]
+    index_location = capture_values.index(first_occurrence)
+    working_value = values[index_location] * first_occurrence
 
     for counter, item in enumerate(capture_values):
+     print(item)
+     
      if item > 0:
-      while count <= item: 
+      while working_value > 0:
        roman_string += numerals[counter]
-       count += 1
+       working_value -= values[index_location]
 
     print(roman_string)
 
-roman_numeral(4)
+roman_numeral(10)
