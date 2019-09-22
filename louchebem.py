78adf042b4855a8a88f122f4d7c003792faccb7f
Thomas Ferris <thomaskeithferris@gmail.com>
2019-09-22 16:49:35 -0700

    update

---
+++ louchebem.py
@@ -0,0 +1,39 @@
+# first consonant group (letters before the first vowel), and replace it with an "L".
+#  Then, you put that first consonant group at the end of the word, followed by one of the louchebem suffixes
+#   So "PATRON" becomes "LATRONPEM"
+
+#words beginning with consonants ("chat", "trou"), you'll have to take the first consonant group 
+#(all the letters before the first vowel) and put it at the end, add an l to the start of the word and add a suffix at the end 
+#("chat" should give "latchem", or "latchoc")
+
+# words beginning with a vowel are not changed but you should still add an l to the start of the word and a suffix at the end (
+    #"atout" should give "latoutoc" or "latoutic")
+
+#should be able to translate any complicated sentence, regardless of punctuation
+
+
+suffix = ["em", "e", "ji", "oc", "ic", "uche", "es"]
+vowels = ["a", "e", "i", "o", "u", "y"]
+
+def louchbem_translator(str):
+ if len(str) < 2:
+  print("Word too small to translate")
+
+ str_list = [x for x in str]
+
+ const_group = []
+ statement = True
+
+ for char in str_list: 
+  for v in vowels:
+    if char == v is False:
+     const_group.append(char)
+     print(const_group)
+     str_list.remove(char)
+    else: 
+     return v
+
+
+ print(const_group)
+
+louchbem_translator("patron")
